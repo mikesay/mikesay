86acb7bc57c78a56ba4823646739d7a74e8f4938
mikejianzhang <mikejianzhang@163.com>
2023-03-19 00:28:25 +0800

    Move donate position.

--- scripts/mikesay-injects.js
+++ scripts/mikesay-injects.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 hexo.extend.filter.register('theme_inject', function(injects) {
-    injects.postComments.file('donate', 'themes/fluid-mikesay/layout/_partials/donate/donate.ejs');
+    injects.postMarkdownEnd.file('donate', 'themes/fluid-mikesay/layout/_partials/donate/donate.ejs');
     injects.style.push('themes/fluid-mikesay/source/css/_donate/donate.styl');
     injects.style.push('themes/fluid-mikesay/source/css/_changyan/changyan.styl');
   });
