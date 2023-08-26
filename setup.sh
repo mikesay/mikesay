9d168f8aa30165972ce644c8c95157b1bbfc90c4
mikejianzhang <mikejianzhang@163.com>
2023-08-26 21:30:37 +0800

    Update background images.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ npm install
 # Customize theme fluid
 echo "Customize theme fluid"
 cp -f themes/fluid-mikesay/source/img/default.png node_modules/hexo-theme-fluid/source/img/
-cp -f themes/fluid-mikesay/source/img/aboutme.jpg node_modules/hexo-theme-fluid/source/img/
+cp -f themes/fluid-mikesay/source/img/aboutme.jpeg node_modules/hexo-theme-fluid/source/img/
 cp -f themes/fluid-mikesay/source/img/alipay.png node_modules/hexo-theme-fluid/source/img/
 cp -f themes/fluid-mikesay/source/img/alipay1.jpeg node_modules/hexo-theme-fluid/source/img/
 cp -f themes/fluid-mikesay/source/img/avatar.png node_modules/hexo-theme-fluid/source/img/
@@ -15,10 +15,13 @@ cp -f themes/fluid-mikesay/source/img/favicon.png node_modules/hexo-theme-fluid/
 cp -f themes/fluid-mikesay/source/img/friends.jpg node_modules/hexo-theme-fluid/source/img/
 cp -f themes/fluid-mikesay/source/img/home.png node_modules/hexo-theme-fluid/source/img/
 cp -f themes/fluid-mikesay/source/img/home_afg.jpeg node_modules/hexo-theme-fluid/source/img/
+cp -f themes/fluid-mikesay/source/img/home_tj.jpg node_modules/hexo-theme-fluid/source/img/
+cp -f themes/fluid-mikesay/source/img/home_tj1.jpg node_modules/hexo-theme-fluid/source/img/
+cp -f themes/fluid-mikesay/source/img/home_tj2.jpeg node_modules/hexo-theme-fluid/source/img/
 cp -f themes/fluid-mikesay/source/img/post.png node_modules/hexo-theme-fluid/source/img/
-cp -f themes/fluid-mikesay/source/img/archive.jpeg node_modules/hexo-theme-fluid/source/img/
-cp -f themes/fluid-mikesay/source/img/category.jpeg node_modules/hexo-theme-fluid/source/img/
-cp -f themes/fluid-mikesay/source/img/tag.jpeg node_modules/hexo-theme-fluid/source/img/
+cp -f themes/fluid-mikesay/source/img/archive.jpg node_modules/hexo-theme-fluid/source/img/
+cp -f themes/fluid-mikesay/source/img/category.jpg node_modules/hexo-theme-fluid/source/img/
+cp -f themes/fluid-mikesay/source/img/tag.jpg node_modules/hexo-theme-fluid/source/img/
 cp -f themes/fluid-mikesay/source/img/wechatpay.png node_modules/hexo-theme-fluid/source/img/
 cp -f themes/fluid-mikesay/source/img/wechatpay1.jpeg node_modules/hexo-theme-fluid/source/img/
 cp -f themes/fluid-mikesay/source/img/wechat_mp.jpg node_modules/hexo-theme-fluid/source/img/
